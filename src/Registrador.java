d1e8127151bcf147461563054dd1808c3a2aa206
GabrielRocha1031 <54642896+GabrielRocha1031@users.noreply.github.com>
2022-05-12 09:55:58 -0300

    Registrador para X, L, B e S

---
+++ src/Registrador.java
@@ -0,0 +1,14 @@
+package ps;
+
+public class Registrador {
+
+  private String conteudo;
+
+  public String getConteudo() {
+    return conteudo;
+  }
+
+  public void setConteudo(String conteudo) {
+    this.conteudo = conteudo;
+  }
+}
