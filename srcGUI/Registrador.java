505980d886c3491f09188d537102bd2e71c36b07
GabrielRocha1031 <54642896+GabrielRocha1031@users.noreply.github.com>
2022-06-21 19:00:12 -0300

    Add files via upload

---
+++ srcGUI/Registrador.java
@@ -0,0 +1,14 @@
+package ps;
+
+public class Registrador extends GUI{
+
+  private int conteudo;
+
+  public int getConteudo() {
+    return conteudo;
+  }
+
+  public void setConteudo(int conteudo) {
+    this.conteudo = conteudo;
+  }
+}
